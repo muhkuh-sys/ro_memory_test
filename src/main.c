539a1805192f72f61865b63af3775a1c007a5d7c
doc_bacardi <devnull@localhost>
2012-09-20 11:35:31 +0200

    use platform library uart functions

--- src/main.c
+++ src/main.c
@@ -3,12 +3,16 @@
 
 #include "rdy_run.h"
 #include "netx_io_areas.h"
+#include "serial_vectors.h"
 #include "systime.h"
 #include "uprintf.h"
 #include "uart.h"
 
 
-static const UART_CONFIGURATION_T tUartCfg_nxhx10_etm =
+
+#if ASIC_TYP==10
+/* NXHX10-ETM */
+static const UART_CONFIGURATION_T tUartCfg =
 {
 	.uc_rx_mmio = 20U,
 	.uc_tx_mmio = 21U,
@@ -16,6 +20,74 @@ static const UART_CONFIGURATION_T tUartCfg_nxhx10_etm =
 	.uc_cts_mmio = 0xffU,
 	.us_baud_div = UART_BAUDRATE_DIV(UART_BAUDRATE_115200)
 };
+#elif ASIC_TYP==56
+/* NXHX51-ETM */
+static const UART_CONFIGURATION_T tUartCfg =
+{
+	.uc_rx_mmio = 34U,
+	.uc_tx_mmio = 35U,
+	.uc_rts_mmio = 0xffU,
+	.uc_cts_mmio = 0xffU,
+	.us_baud_div = UART_BAUDRATE_DIV(UART_BAUDRATE_115200)
+};
+#elif ASIC_TYP==50
+/* NXHX50-ETM */
+static const UART_CONFIGURATION_T tUartCfg =
+{
+	.uc_rx_mmio = 34U,
+	.uc_tx_mmio = 35U,
+	.uc_rts_mmio = 0xffU,
+	.uc_cts_mmio = 0xffU,
+	.us_baud_div = UART_BAUDRATE_DIV(UART_BAUDRATE_115200)
+};
+#elif ASIC_TYP==100 || ASIC_TYP==500
+static const UART_CONFIGURATION_T tUartCfg =
+{
+	.us_baud_div = UART_BAUDRATE_DIV(UART_BAUDRATE_115200)
+};
+#endif
+
+
+#define IO_UART_UNIT 0
+
+static unsigned char io_uart_get(void)
+{
+	return (unsigned char)uart_get(IO_UART_UNIT);
+}
+
+
+static void io_uart_put(unsigned int uiChar)
+{
+	uart_put(IO_UART_UNIT, (unsigned char)uiChar);
+}
+
+
+static unsigned int io_uart_peek(void)
+{
+	return uart_peek(IO_UART_UNIT);
+}
+
+
+static void io_uart_flush(void)
+{
+	uart_flush(IO_UART_UNIT);
+}
+
+
+static const SERIAL_COMM_UI_FN_T tSerialVectors_Uart =
+{
+	.fn =
+	{
+		.fnGet = io_uart_get,
+		.fnPut = io_uart_put,
+		.fnPeek = io_uart_peek,
+		.fnFlush = io_uart_flush
+	}
+};
+
+
+SERIAL_COMM_UI_FN_T tSerialVectors;
+
 
 extern unsigned long _binary_test_bin_end[];
 extern unsigned long _binary_test_bin_start[];
@@ -33,7 +105,10 @@ void test_main(void)
 
 
 	systime_init();
-	uart_init(0, &tUartCfg_nxhx10_etm);
+	uart_init(0, &tUartCfg);
+
+	/* Set the serial vectors. */
+	memcpy(&tSerialVectors, &tSerialVectors_Uart, sizeof(SERIAL_COMM_UI_FN_T));
 
 	uprintf("*** hallo! ***\n");
 
